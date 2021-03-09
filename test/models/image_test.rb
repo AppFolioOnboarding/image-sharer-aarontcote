b31479dfcaffc9f127224456319f0838a1cc6e03
Aaron Cote <aaron.cote@appfolio.com>
2021-03-09 13:12:36 -0800

    Add image model tests

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should not save empty image url" do
+    image = Image.new
+    assert_not image.save
+  end
+
+  test "should not save short image url" do
+    image = Image.new
+    image.url = "http://i."
+    assert_not image.save
+  end
+
+  test "should not save long image url" do
+    image = Image.new
+    image.url = "http://www.loremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsedloremipsumdolorsitametconsecteturadipiscingelitsed.com/image.png"
+    assert_not image.save
+  end
+
+  test "should not save invalid image url" do
+    image = Image.new
+    image.url = "htp:/www.image.com"
+    assert_not image.save
+  end
+
+  test "should save valid image url" do
+    image = Image.new
+    image.url = "https://www.appfolio.com/_nuxt/img/logo-apm-horizontal.6400026.svg"
+    assert image.save
+  end
+  
+
 end

80573854817ed5ddaee99e4b02fc538b3b8b9c80
Aaron Cote <aaron.cote@appfolio.com>
2021-03-10 16:56:38 -0800

    Update the image controller/view tests to check for form load, specific success response, and failed image url save

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
+  test 'should show image url create form' do
+    get new_image_path
+    assert_response :ok
+
+    assert_select 'input' do
+      assert_select '[name=?]', 'image[url]'
+      assert_select '[name=?]', 'commit'
+    end
+  end
+
   test 'should create an image url' do
     assert_difference 'Image.count' do
       post images_path, params: { image: { url: 'https://pa.cdn.appfolio.com/appfolio/images/apm-white.svg' } }
@@ -11,10 +21,16 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   test 'should show an image from the url' do
     created_image_url = Image.create(url: 'https://www.appfolio.com/_nuxt/img/logo-apm-horizontal.6400026.svg')
     get image_path(created_image_url.id)
-    assert_response :success
+    assert_response :ok
 
     assert_select 'img' do
       assert_select '[src=?]', 'https://www.appfolio.com/_nuxt/img/logo-apm-horizontal.6400026.svg'
+    end
+  end
+
+  test 'should prevent image url from saving' do
+    assert_difference 'Image.count', 0 do
+      post images_path, params: { image: { url: '' } }
     end
   end
 end

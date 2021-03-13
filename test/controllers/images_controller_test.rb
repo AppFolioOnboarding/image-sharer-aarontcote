be7fc527cb9d80301e2513af4540e082219c5863
Aaron Cote <aaron.cote@appfolio.com>
2021-03-12 16:21:51 -0800

    Improve image test structure and flexbility

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should show all images' do
+  # index view tests
+  test 'index should show all images' do
+    setup_sample_image_data
+
     get images_path
     assert_response :ok
 
+    page_image_count = 0
     source_image_count = Image.all.count
 
-    if source_image_count.positive?
-      page_image_count = 0
-
-      assert_select 'div.card-body' do |elements|
-        elements.each do
-          page_image_count += 1
-        end
+    assert_select 'div.card-body' do |elements|
+      elements.each do
+        page_image_count += 1
       end
     end
-    assert_equal(source_image_count, page_image_count)
+    assert_equal source_image_count, page_image_count
   end
 
-  test 'should list newest image first' do
-    Image.create(url: 'https://pa.cdn.appfolio.com/appfolio/images/apm-white.svg')
+  test 'index should list newest image first' do
+    setup_sample_image_data
 
     get images_path
     assert_response :ok
@@ -30,7 +30,9 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'should have all images limited to 400px' do
+  test 'index should have all images limited to 400px' do
+    setup_sample_image_data
+
     get images_path
     assert_response :ok
 
@@ -43,7 +45,8 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'should show image url create form' do
+  # create view tests
+  test 'create should show image url create form' do
     get new_image_path
     assert_response :ok
 
@@ -53,24 +56,14 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'should create an image url' do
+  test 'create should create an image url' do
     assert_difference 'Image.count' do
       post images_path, params: { image: { url: 'https://pa.cdn.appfolio.com/appfolio/images/apm-white.svg' } }
       assert_redirected_to image_path(Image.last)
     end
   end
 
-  test 'should show an image from the url' do
-    created_image_url = Image.create(url: 'https://www.appfolio.com/_nuxt/img/logo-apm-horizontal.6400026.svg')
-    get image_path(created_image_url.id)
-    assert_response :ok
-
-    assert_select 'img' do
-      assert_select '[src=?]', 'https://www.appfolio.com/_nuxt/img/logo-apm-horizontal.6400026.svg'
-    end
-  end
-
-  test 'should prevent image url from saving' do
+  test 'create should prevent image url from saving' do
     assert_difference 'Image.count', 0 do
       post images_path, params: { image: { url: '' } }
     end
@@ -80,5 +73,26 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'ul' do
       assert_select '[id=?]', 'errorList'
     end
+  end
+
+  # show view tests
+  test 'show should show an image from the url' do
+    created_image_url = Image.create(url: 'https://www.appfolio.com/_nuxt/img/logo-apm-horizontal.6400026.svg')
+    get image_path(created_image_url.id)
+    assert_response :ok
+
+    assert_select 'img' do
+      assert_select '[src=?]', 'https://www.appfolio.com/_nuxt/img/logo-apm-horizontal.6400026.svg'
+    end
+  end
+
+  private
+
+  def setup_sample_image_data
+    Image.delete_all
+
+    Image.create(url: 'https://pa.cdn.appfolio.com/appfolio/images/apm-white.svg')
+    Image.create(url: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png')
+    Image.create(url: 'https://c0.wallpaperflare.com/preview/107/750/505/thor-s-well-united-states-yachats-pnw.jpg')
   end
 end
